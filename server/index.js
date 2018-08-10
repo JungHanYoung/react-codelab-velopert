05286ccbad90ca112f9627db813eec99b80b4887
Hanyoung Jeong <8735132@gmail.com>
2018-08-10 15:45:53 +0900

    es6 컴파일 관련 라이브러리 설치 및 서버 엔트리파일  문법변경

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
-const express = require('express');
+import express from 'express';
 
 const app = express();
 
 const server_port = 3000;
+
+app.get('/', (req, res) => {
+	res.send('hello');
+});
 
 app.listen(server_port, () => console.log(`Server is running on port ${server_port}`));
