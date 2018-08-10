aed0796178e9175fd9eeb97db3b92ac30c83fa34
Hanyoung Jeong <8735132@gmail.com>
2018-08-10 16:26:38 +0900

    mongoose 모듈 설치 및 호스팅한 mongodb 연결확인

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
+
+const db = mongoose.connection;
+db.on('error', console.error);
+db.on('open', () => console.log('Connected to MongoDB Server...'));
+
+mongoose.connect('mongodb://tester:test123@ds145951.mlab.com:45951/memopad_velopert', {
+	useNewUrlParser: true
+});
 
 const server_port = 3000;
 
